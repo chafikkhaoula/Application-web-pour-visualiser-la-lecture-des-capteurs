cd3c05b94f75b4d9768941da0959a98383f7f307
chafikkhaoula <86022808+chafikkhaoula@users.noreply.github.com>
2022-07-08 14:35:06 +0100

    Add files via upload

---
+++ public/scripts/charts-definition.js
@@ -0,0 +1,130 @@
+// Create the charts when the web page loads
+window.addEventListener('load', onload);
+
+function onload(event){
+  chartT = createTemperatureChart();
+  chartH = createHumidityChart();
+  chartP = createHumidityofsoilChart();
+}
+
+// Create Temperature Chart
+function createTemperatureChart() {
+  var chart = new Highcharts.Chart({
+    chart:{ 
+      renderTo:'chart-temperature',
+      type: 'spline' 
+    },
+    series: [
+      {
+        name: 'DHT22'
+      }
+    ],
+    title: { 
+      text: undefined
+    },
+    plotOptions: {
+      line: { 
+        animation: false,
+        dataLabels: { 
+          enabled: true 
+        }
+      },
+      series: { 
+        color: '#f44336' 
+      }
+    },
+    xAxis: {
+      type: 'datetime',
+      dateTimeLabelFormats: { second: '%H:%M:%S' }
+    },
+    yAxis: {
+      title: { 
+        text: 'Degrés Celsius de température' 
+      }
+    },
+    credits: { 
+      enabled: false 
+    }
+  });
+  return chart;
+}
+
+// Create Humidity Chart
+function createHumidityChart(){
+  var chart = new Highcharts.Chart({
+    chart:{ 
+      renderTo:'chart-humidity',
+      type: 'spline'  
+    },
+    series: [{
+      name: 'DHT22'
+    }],
+    title: { 
+      text: undefined
+    },    
+    plotOptions: {
+      line: { 
+        animation: false,
+        dataLabels: { 
+          enabled: true 
+        }
+      },
+      series: { 
+        color: '#50b8b4' 
+      }
+    },
+    xAxis: {
+      type: 'datetime',
+      dateTimeLabelFormats: { second: '%H:%M:%S' }
+    },
+    yAxis: {
+      title: { 
+        text: 'Humidité (%)' 
+      }
+    },
+    credits: { 
+      enabled: false 
+    }
+  });
+  return chart;
+}
+
+// Create humidity of soil Chart
+function createHumidityofsoilChart() {
+  var chart = new Highcharts.Chart({
+    chart:{ 
+      renderTo:'chart-humditysoil',
+      type: 'spline'  
+    },
+    series: [{
+      name: 'Capteur d"humidité du sol'
+    }],
+    title: { 
+      text: undefined
+    },    
+    plotOptions: {
+      line: { 
+        animation: false,
+        dataLabels: { 
+          enabled: true 
+        }
+      },
+      series: { 
+        color: '#1ba577' 
+      }
+    },
+    xAxis: {
+      type: 'datetime',
+      dateTimeLabelFormats: { second: '%H:%M:%S' }
+    },
+    yAxis: {
+      title: { 
+        text: 'Humidité du sol (%)' 
+      }
+    },
+    credits: { 
+      enabled: false 
+    }
+  });
+  return chart;
+}
